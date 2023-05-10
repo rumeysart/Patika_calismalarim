cc7ad470d470b946a9ff22f8b4fe357f0c8c6c85
Rümeysa TATAR <111748034+rumeysart@users.noreply.github.com>
2023-05-10 16:15:11 +0300

    Update odev10.sql

--- postgreSql/join/odev10.sql
+++ postgreSql/join/odev10.sql
@@ -6,7 +6,12 @@ LEFT JOIN country ON city.country_id = country.country_id;
 
 --customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte 
 --görebileceğimiz RIGHT JOIN sorgusunu yazınız.
+SELECT first_name, last_name, payment_id FROM customer
+RIGHT JOIN payment ON customer.customer_id = payment.customer_id;
 
 
 /*customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte 
 görebileceğimiz FULL JOIN sorgusunu yazınız.*/
+SELECT first_name, last_name, rental_id FROM customer
+FULL JOIN rental ON customer.customer_id = rental.rental_id;
+
