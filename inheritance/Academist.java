3bd910c4a7ea1c51b3a4ab3a44b20d53df5007bd
Rümeysa TATAR <111748034+rumeysart@users.noreply.github.com>
2023-11-17 12:16:01 +0300

    Add files via upload

---
+++ inheritance/Academist.java
@@ -0,0 +1,40 @@
+package inheritance;
+
+public class Academist extends Workers {
+//kalıtımda bir sınıf bir sınıfı extends ediyorsa
+//atası olan sınıfta constructorlar varsa aynısını 
+	//çağırmak zorundadır der
+
+	private String position;
+	private String section;
+	public Academist(String nameSurname, String mp, String eposta, String position, String section) 
+{//değişkenleri üst sınıftan miras alıyoruz
+		super(nameSurname, mp, eposta);
+//ata sınıftaki constructor boş olsaydı hiçbir şey yazmamıza gerek yoktu
+		this.section = section;
+		this.position = position;
+}
+	public String getPosition() {
+		return position;
+	}
+	public String getSection() {
+		return section;
+	}
+	public void setPosition(String position) {
+		this.position = position;
+	}
+	public void setSection(String section) {
+		this.section = section;
+	}
+	
+	public void gotoClass() {
+		System.out.println(this.getNameSurname() + " entered to the Class");
+		
+		
+	}
+	@Override
+	public void enter() {
+		System.out.println(this.getNameSurname() + " academist is enter from A Port");
+	}
+
+}
